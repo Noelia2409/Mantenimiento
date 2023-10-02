f80e6d28a018628d0e5599d9f0387282e48df6cf
Noelia <117768747+Noelia2409@users.noreply.github.com>
2023-10-02 14:15:36 -0500

    Add files via upload

---
+++ js.js/trabajo.js
@@ -0,0 +1,14 @@
+window.addEventListener('click', ()=>{
+    console.log(e. target)
+})
+
+const imagenes= document.querySelectorAll('.img-portafolio')
+const imagenesdos=document.querySelector('agregar-imagen')
+const contenedorLight= document.querySelector('.imagen-light')
+
+portafolio.forEach(imagen =>{
+   imagen.addEventListener ('click', ()=>{
+    alert("esta es una imagen")
+   })
+)
+
